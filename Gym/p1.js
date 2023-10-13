3c78faa4b39d0676f3e17b601372a246ed1a8482
sanskar-decoder <72738470+sanskar-decoder@users.noreply.github.com>
2023-10-13 18:58:56 +0530

    Add files via upload

---
+++ Gym/p1.js
@@ -0,0 +1,43 @@
+var tl=gsap.timeline();
+
+tl.from(" .page1 .half1 h1",{
+
+    y:-15,
+    duration:2,
+    opacity :0
+    
+})
+
+
+tl.to(" .halfb .b1",{
+
+    y:-15,
+    duration:1
+    
+    
+    
+})
+
+tl.from(" .halfb .b",{
+
+    y:-15,
+    duration:1,
+    opacity :0
+   
+    
+    
+    
+})
+
+
+
+tl.from(".page1 .cont h1,h2,span",{
+
+    y:-15,
+    duration:1,
+    opacity :0
+    
+    
+})
+
+
